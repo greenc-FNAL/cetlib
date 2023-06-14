3419fa0ff8b54442c5835742b11c0942b52cc716
Zachary Evans <ztevans@fnal.gov>
2023-06-14 11:03:56 -0500

    Integrates concepts into hard_cast.h with tests

--- cetlib/hard_cast.h
+++ cetlib/hard_cast.h
@@ -1,20 +1,44 @@
 #ifndef cetlib_hard_cast_h
 #define cetlib_hard_cast_h
 
+#include "cetlib_except/cxx20_macros.h"
+#if CET_CONCEPTS_AVAILABLE
+#include <concepts>
+#endif
 #include <cstring>
+
+#if CET_CONCEPTS_AVAILABLE
+namespace detail {
+  template <typename T>
+  concept cet_pointer = std::is_pointer_v<T>;
+}
+#endif
 
 namespace cet {
   // For use when only a C++ {dynamic,static,reinterpret}_cast is not
   // sufficient to the task. The only case of this known currently is
   // when using dlopen, dlsym, etc. and a void * must be cast to a
   // function pointer.
+  #if CET_CONCEPTS_AVAILABLE
+  template <detail::cet_pointer PTR_T>
+  #else
   template <typename PTR_T>
+  #endif
   PTR_T hard_cast(void* src);
+
+  #if CET_CONCEPTS_AVAILABLE
+  template <detail::cet_pointer PTR_T>
+  #else
   template <typename PTR_T>
+  #endif
   void hard_cast(void* src, PTR_T& dest);
 }
 
+#if CET_CONCEPTS_AVAILABLE
+template <detail::cet_pointer PTR_T>
+#else
 template <typename PTR_T>
+#endif
 inline PTR_T
 cet::hard_cast(void* src)
 {
@@ -23,7 +47,11 @@ cet::hard_cast(void* src)
   return dest;
 }
 
+#if CET_CONCEPTS_AVAILABLE
+template <detail::cet_pointer PTR_T>
+#else
 template <typename PTR_T>
+#endif
 inline void
 cet::hard_cast(void* src, PTR_T& dest)
 {
---
+++ cetlib/test/bad_hard_cast_t.cc
@@ -0,0 +1,7 @@
+#include "cetlib/hard_cast.h"
+
+int main() {
+  [[maybe_unused]] int value = 23;
+  int* result = cet::hard_cast<int>(value);
+  return *result;
+}
