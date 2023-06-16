86062f7da13c311afcd434ae6f5daedea4ae338c
Chris Green <greenc@fnal.gov>
2023-06-16 11:17:07 -0500

    Test expected compile failures in `container_algorithms.h`
    Evaluation is that enhancement of error messages via
    concepts/constraints in `container_algorithms.h` is not beneficial at
    this time.

---
+++ cetlib/test/bad_for_all_container_t.cc
@@ -0,0 +1,11 @@
+#include "cetlib/container_algorithms.h"
+
+template <typename T>
+struct bad_container {
+  T t;
+};
+
+int main() {
+  bad_container<int> bc;
+  cet::for_all(bc, [](int b) { return b; });
+}
