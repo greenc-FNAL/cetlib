fe04565e7deae06d9fb8a30bb6f294be55e44db6
Chris Green <greenc@fnal.gov>
2023-06-28 11:24:42 -0500

    Format and tidy

--- cetlib/test/bad_value_ptr_t.cc
+++ cetlib/test/bad_value_ptr_t.cc
@@ -2,20 +2,19 @@
 
 #include <cstdint>
 
-union simple_base{
-  public:
-    std::int32_t n;
-    std::uint16_t s;
-    std::uint8_t c;
-    void clone(){
-      
-    }
+union simple_base {
+public:
+  std::int32_t n;
+  std::uint16_t s;
+  std::uint8_t c;
+  void
+  clone()
+  {}
 };
 
-
-int main(){
+int
+main()
+{
   int my_int = 3;
   cet::value_ptr<simple_base> p(&my_int);
-  //static_assert(cet::_::WouldSlice<simple_base, cet::default_clone<simple_base>, simple_base>);
-  //static_assert(cet::_::PolymorphicWithClone<simple_base>);
 }
--- cetlib/test/bad_value_ptr_t_2.cc
+++ cetlib/test/bad_value_ptr_t_2.cc
@@ -2,21 +2,20 @@
 
 #include <cstdint>
 
-struct simple_base{
+struct simple_base {
   std::int32_t n;
   std::uint16_t s;
   std::uint8_t c;
   virtual ~simple_base() noexcept = default;
 };
 
-struct simple_sub : simple_base{
+struct simple_sub : public simple_base {
   std::int32_t m;
 };
 
-
-int main(){
+int
+main()
+{
   simple_sub s1;
   cet::value_ptr<simple_base> p(&s1);
-  //static_assert(cet::_::WouldSlice<simple_base, cet::default_clone<simple_base>, simple_base>);
-  //static_assert(cet::_::PolymorphicWithClone<simple_base>);
 }
