d47b7a5a39b301a94283828a9610cce2839f306e
Zachary Evans <ztevans@fnal.gov>
2023-06-30 10:06:19 -0500

    tests for `registry_via_id.h` and non-working `CMakeLists.txt`

--- cetlib/test/bad_reg_via_id_t_2.cc
+++ cetlib/test/bad_reg_via_id_t_2.cc
@@ -4,18 +4,19 @@ using cet::registry_via_id;
 
 /*
 * This test is meant to fail due to the registry
-* being used without a proper id() function in 
-* the value type
+* being used and having an id() function for its
+* value type, but the id() function returns the
+* wrong type.
 */
 
 struct thing {
   int i;
   thing(int i = 0) : i(i) {}
-  // float
-  // id() const
-  // {
-  //   return i;
-  // }
+  float
+  id() const
+  {
+    return i;
+  }
   bool
   operator==(int other) const
   {
@@ -25,5 +26,5 @@ struct thing {
 
 
 int main(){
-  using reg = registry_via_id<float, thing>;
+  using reg = registry_via_id<char, thing>;
 }
