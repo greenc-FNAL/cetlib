3eff2480ad8c055cef02ce8a1e69411152583e95
Chris Green <greenc@fnal.gov>
2023-07-05 10:05:59 -0500

    Move bad_col_t to sqlite/

--- cetlib/test/sqlite/bad_col_t.cc
+++ cetlib/test/sqlite/bad_col_t.cc
@@ -6,8 +6,7 @@
 
 using namespace cet::sqlite;
 
-struct nothing {
-
+struct no_good_type {
 };
 
 int main(){
@@ -15,5 +14,5 @@ int main(){
 
   std::string const f{ "a.db" };
   std::unique_ptr<Connection> c{cf.make_connection(f)};
-  create_table(*c, "onlyOne", column<nothing>{"something"});
+  create_table(*c, "onlyOne", column<no_good_type>{"something"});
   }
